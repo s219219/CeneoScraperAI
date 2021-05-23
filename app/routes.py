0fb02f76a4ac3ef158085087e293b365afa5301f
Kirill Likholietov <kirilllikholietov@MacBook-Air-Kirill.local>
2021-05-23 15:54:35 +0200

    20.05.2021

--- app/routes.py
+++ app/routes.py
@@ -6,16 +6,17 @@ from flask import request, render_template, redirect, url_for
 import requests
 import json
 
+app.config['SECRET_KEY'] = "NotSoSecretKey"
+
 @app.route('/')
 @app.route('/index')
 def index():
-    opinion1 = Opinion()
-    return str(opinion1)
+    return render_template('main.html.jinja')
 
 @app.route('/extract', methods=['GET', 'POST'])
 def extract():
     form = ProductForm()
-    if request.method == 'POST' and form.validate():
+    if request.method == 'POST' and form.validate_on_submit():
         product = Product(request.form['productId'])
         respons = requests.get(product.opinionsPageUrl())
         if respons.status_code == 200:
@@ -24,7 +25,7 @@ def extract():
             return redirect(url_for('product', productId=product.productId))
         else:
             form.productId.errors.append("For given productId there is no product")
-    return render_template('extract.html', form=form)
+    return render_template('extract.html.jinja', form=form)
 
 @app.route('/product/<productId>')
 def product(productId):
